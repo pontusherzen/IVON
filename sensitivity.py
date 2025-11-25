51bfa05d1b97c5f3989ebf63bcc51e28330c8a28
Adam Johansson <adjohan@kth.se>
2025-11-25 14:24:35 +0100

    Cleaned up scaffolding in sensitivity code and improved documentation

--- sensitivity.py
+++ sensitivity.py
@@ -19,6 +19,7 @@ from tqdm import tqdm
 from typing import Tuple
 
 
+# Create hyperparameter configs for each considered dataset
 
 CIFAR10_CONFIG = OmegaConf.create({
     "lr": 0.2,
@@ -57,6 +58,9 @@ NORM_STD = torch.tensor([0.229, 0.224, 0.225])
 
 
 def make_dataset(name: str, root: pathlib.Path = pathlib.Path("data")) -> Tuple[Dataset, Dataset]:
+    """
+    Loads the dataset with name 'name' and returns a tuple (train_set, val_set)
+    """
     if name == "cifar10":
         transform = Compose([
             ToImage(),
@@ -70,12 +74,21 @@ def make_dataset(name: str, root: pathlib.Path = pathlib.Path("data")) -> Tuple[
         raise NotImplementedError()
 
 def denormalize(img_tensor: torch.Tensor) -> torch.Tensor:
+    """
+    Takes in a normalized image tensor of shape 
+    (batch, 3, height, width)
+    and returns a denormalized image tensor of shape
+    (batch, height, width, 3)
+    """
     img_01 = img_tensor * NORM_STD[None, :, None, None] + NORM_MEAN[None, :, None, None]
     img_01 = img_01.clamp(min=0.0, max=1.0).permute(0, 2, 3, 1)
     return img_01
 
 
 def init_model(name: str, n_classes: int) -> nn.Module:
+    """
+    Initializes a torch model based on the name of the architecture and the number of ouput classes
+    """
     if name == "resnet18":
         model = torchvision.models.resnet18()
         model.fc = nn.Linear(model.fc.in_features, n_classes)
@@ -111,6 +124,9 @@ def load_checkpoint(model: nn.Module, optimizer: torch.optim.Optimizer,
 
 
 def train(args, model: nn.Module, optimizer: IVON, train_loader: DataLoader[Tuple[torch.Tensor, torch.Tensor]], config: DictConfig):
+    """
+    Runs a training loop, with hyperparameters 'config'
+    """
     device = torch.device("cuda:0")
 
     schedule = LinearLR(optimizer, start_factor = 1/config.warmup_epochs, total_iters=config.warmup_epochs)
@@ -137,23 +153,43 @@ def train(args, model: nn.Module, optimizer: IVON, train_loader: DataLoader[Tupl
 
 @torch.no_grad()
 def get_error_batch(x: torch.Tensor, y: torch.Tensor, model: nn.Module, n_classes: int) -> torch.Tensor:
+    """
+    Get the error estimate on a batch of example inputs x of shape (batch, *) with corresponding true labels y of shape (batch,)
+    Returns a tensor of shape (batch, n_classes)
+
+    Follows definitions in appendix C.5. of the IVON paper.
+    """
     logits = model(x)
     p = nn.functional.softmax(logits, dim=-1)
     error = p - nn.functional.one_hot(y, num_classes=n_classes)
     return error
 
-def get_sensitivity_batch(x: torch.Tensor, model: nn.Module, ivon: IVON, n_classes: int) -> torch.Tensor:
+def get_variance_batch(x: torch.Tensor, model: nn.Module, ivon: IVON, n_classes: int) -> torch.Tensor:
+    """
+    Get the variance estimate as described in appendix C.5. of the IVON paper for a batch of inputs x of shape (batch, *).
+    Uses the optimizer state of the IVON optimizer to retrieve the relevant variances.
+    Returns a tensor of shape (batch, n_classes, n_classes).
+    """
     sub_results = []
     for i in range(x.size(0)):
-        sub_results.append(get_sensitivity_single(x=x[i], model=model, ivon=ivon, n_classes=n_classes))
+        sub_results.append(get_variance_single(x=x[i], model=model, ivon=ivon, n_classes=n_classes))
     return torch.stack(sub_results, dim=0)
 
 
-def get_sensitivity_single(x: torch.Tensor, model: nn.Module, ivon: IVON, n_classes: int) -> torch.Tensor:
+def get_variance_single(x: torch.Tensor, model: nn.Module, ivon: IVON, n_classes: int) -> torch.Tensor:
+    """
+    Get the variance estimate described in appendix C.5. of the IVON paper for a single input x.
+    Uses the optimizer state of the IVON optimizer to retrieve the relevant variances.
+    Returns a tensor of shape (n_classes, n_classes).
+    """
 
+    # Initialize buffers to hold the output jacobian of shape (n_params, n_classes)
     jacobian = torch.zeros(0, n_classes, dtype=x.dtype, device=x.device)
+
+    # Initialize buffer to hold the variance (squared sigma) for each parameter as a vector of shape (n_params,)
     sigma2 = torch.zeros(0, dtype=x.dtype, device=x.device)
 
+    # Loop through all parameters to fill the jacobian and sigma buffers
     for group in ivon.param_groups:
         for param in group["params"]:
             this_param_jacobian_shape = (param.numel(), n_classes)
@@ -162,6 +198,7 @@ def get_sensitivity_single(x: torch.Tensor, model: nn.Module, ivon: IVON, n_clas
         this_sigma2 = 1.0 / group["ess"] * (group["hess"] + group["weight_decay"])
         sigma2 = torch.cat((sigma2, this_sigma2))
 
+    # Number of parameters should be consistent with the size of the buffers
     n_parameters = jacobian.shape[0]
     assert sigma2.shape == (n_parameters,)
 
@@ -169,13 +206,14 @@ def get_sensitivity_single(x: torch.Tensor, model: nn.Module, ivon: IVON, n_clas
     for i in range(n_classes):
         output = model(x[None, ...]) 
 
-        # Get i:th row of jacobian
-        metric = output[0, i]
+        # Get i:th row of jacobian == derivative of i:th output logit
+        metric = output.squeeze(0)[i]
         assert metric.shape == ()
 
         ivon.zero_grad()
         metric.backward()
 
+        # Walk through the gradient parameter by parameter and fill the relevant sections of the jacobian
         param_i = 0
         for param in model.parameters():
             assert param.grad is not None
@@ -184,6 +222,8 @@ def get_sensitivity_single(x: torch.Tensor, model: nn.Module, ivon: IVON, n_clas
             param_i += flattened_grad.numel()
         assert param_i == n_parameters
 
+    # Do the contraction.
+    # We use einsum for the sake of memory efficiency since the matrices are large.
     # V = J^T @ diag(sigma²) @ J   <--> einsum("pc, pq, dq", jacobian, diag(sigma2), jacobian)  <--> einsum("pc,p,pd", jacobian, sigma2, jacobian)
     v = torch.einsum("pc,p,pd", jacobian, sigma2, jacobian)
     assert v.shape == (n_classes, n_classes)
@@ -191,9 +231,11 @@ def get_sensitivity_single(x: torch.Tensor, model: nn.Module, ivon: IVON, n_clas
 
 
 def train_session(args):
+    """
+    Run a full training run based on passed user args
+    """
     device = torch.device("cuda:0")
-    train_set, val_set = make_dataset(args.dataset_name, root=pathlib.Path("/ml/data/"))
-    train_set = Subset(train_set, list(range(10000)))
+    train_set, val_set = make_dataset(args.dataset_name, root=args.data_root)
 
     config = CONFIGS[args.dataset_name]
     train_loader = DataLoader(train_set, batch_size=config.batch, shuffle=True)
@@ -204,10 +246,13 @@ def train_session(args):
     train(args, model, optimizer, train_loader, config)
 
 def eval_session(args):
+    """
+    Runn a full sensitivity eval session based on passed user args
+    """
     device = torch.device("cuda:0")
-    train_set, val_set = make_dataset(args.dataset_name, root=pathlib.Path("/ml/data/"))
+    train_set, val_set = make_dataset(args.dataset_name, root=args.data_root)
 
-    single_category = 9
+    single_category = args.showcase_category
     single_category_val_set = Subset(val_set, list(i for i in range(len(val_set)) if val_set[i][1] == single_category))
 
     config = CONFIGS[args.dataset_name]
@@ -228,14 +273,13 @@ def eval_session(args):
     for i, (x, y) in enumerate(tqdm(single_category_loader, desc="Evaluating sensitivities")):
         x = x.to(device)
         y = y.to(device)
-        v = get_sensitivity_batch(x=x, model=model, ivon=optimizer, n_classes=config.n_classes)
+        v = get_variance_batch(x=x, model=model, ivon=optimizer, n_classes=config.n_classes)
         e = get_error_batch(x=x, y=y, model=model, n_classes=config.n_classes)
 
         sensitivity = v @ e[:, :, None]  # Shape (batch, nc, nc) @ (batch, nc, 1) -> (batch, nc, 1)
         sensitivity = sensitivity.squeeze(-1)  # Shape (batch, nc)
         sensitivity = sensitivity.norm(2, dim=-1)  # Shape (batch,)
         all_sensitivities += sensitivity.cpu().numpy().tolist()
-
 
     plt.hist(all_sensitivities)
     plt.xlabel("Sensitivity")
@@ -277,12 +321,22 @@ def eval_session(args):
 
 
 if __name__ == '__main__':
+    # Example arguments
+
+    # To train with default dataset/model:
+    # python3 sensitivity.py --train
+
+    # To eval on CIFAR10 (default) showcasing the first class (airplane)
+    # python3 sensitivity.py -m cifar10 --showcase-category 0
+
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train", action="store_true", default=False, help="Flag to train the model.")
-    parser.add_argument("--no-eval", action="store_true", default=False, help="Flag to only train, with no evaluation.")
-    parser.add_argument("-m", "--model-name", type=str, default="resnet18", help="Name of model architecture to train.")
-    parser.add_argument("-d", "--dataset-name", type=str, default="cifar10", help="Name of dataset to train.")
+    parser.add_argument("--train", action="store_true", default=False, help="Flag to train the model. (default=%(default)s)")
+    parser.add_argument("--no-eval", action="store_true", default=False, help="Flag to only train, with no evaluation. (default=%(default)s)")
+    parser.add_argument("-m", "--model-name", type=str, default="resnet18", help="Name of model architecture to train. (default=%(default)s)")
+    parser.add_argument("-d", "--dataset-name", type=str, default="cifar10", help="Name of dataset to train. (default=%(default)s)")
+    parser.add_argument("--data-root", type=pathlib.Path, default=pathlib.Path("./data"), help="Directory where datasets are downloaded to. (default = '%(default)s')")
+    parser.add_argument("--showcase-category", type=int, default=9, help="Index of category to use when getting example images with high/low sensitivity. (default=%(default)s)")
 
     args = parser.parse_args()
     if args.train:
